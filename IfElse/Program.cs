f4b2d690808b7b1cecfeb4c25e51797ad5e1c75a
yaswanth <yaswanth@turingsystems.com>
2022-02-07 15:53:47 +0530

    Add project files.

---
+++ IfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 11;
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("even number");
+            }
+            else 
+                {
+                Console.WriteLine("odd number");
+            }
+        }
+    }
+}
